6631b070e6f17bdd3b84e69292f96719faffb40c
Pablo Espinosa <espipj@gmail.com>
2017-05-02 10:14:45 +0200

    added translation for orders to the robot

--- src/VisionQR.cpp
+++ src/VisionQR.cpp
@@ -23,12 +23,37 @@ int calcularDesplazamientoVertical(cv::Point ai, cv::Point ad,cv::Mat src){
 	//Da negativo, el robot deberia girar/mirar hacia arriba
 	return distarriba-distabajo;
 }
+
+std::string traducirVertical(int d){
+	/* Usamos un rango para tener un margen de "no movimiento" */
+	int rango=25;
+	if(d<(-rango)){
+		return "Retroceder hacia atrás";
+	}else if(d>rango){
+		return "Avanzar hacia delante";
+	}else{
+		return "Correcto";
+	}
+}
+
+std::string traducirHorizontal(int d){
+	/* Usamos un rango para tener un margen de "no movimiento" */
+	int rango=25;
+	if(d<(-rango)){
+		return "Avanzar hacia la derecha";
+	}else if(d>rango){
+		return "Avanzar hacia la izquierda";
+	}else{
+		return "Correcto";
+	}
+}
 bool calcularTamano(cv::Rect r, cv::Mat src){
-	/* Tam=180 el tamaño que creemos que sería optimo para leer el cuadrado del BIDI
+	/* Tam=src.cols/3 (un tercio del tamaño total de la imagen el tamaño que creemos 
+	que sería optimo para leer el cuadrado del BIDI
 	Rango=+-20 es el rango admisible de diferencia respecto al tamaño que queremos*/ 
 	float tam=src.cols/3;
 	int rango=20;
-	std::cout << "Diferencia:" << tam-r.width;
+	//std::cout << "Diferencia:" << tam-r.width;
 	if((tam-r.width)<rango && (tam-r.width)>(-rango)){
 		return true;
 	}else{
@@ -38,18 +63,24 @@ bool calcularTamano(cv::Rect r, cv::Mat src){
 }
 void imprimirResultados(std::vector<cv::Point>& contour, cv::Mat src){
 	cv::Rect r = cv::boundingRect(contour);
-	
-	std::cout << "Ancho: " << r.width << "\n";
-	std::cout << "Alto: " << r.height << "\n";
+	std::cout << "+-------DATOS DE LA IMAGEN-------+\n";
 	std::cout << "Largo Imagen: " << src.cols << "\n";
 	std::cout << "Ancho Imagen: " << src.rows << "\n";
+
+	std::cout << "+----------DATOS DEL QR----------+\n";	
+	std::cout << "Ancho: " << r.width << "\n";
+	std::cout << "Alto: " << r.height << "\n";
 	std::cout << "Vertice Arriba-Izquierda X: " << r.x << " Vertice Arriba-Izquierda Y: " << r.y << "\n";
 	std::cout << "Vertice Abajo-Derecha X: " << calcularVerticeAbajoIzq(r).x << " Vertice Abajo-Derecha Y: " << calcularVerticeAbajoIzq(r).y << "\n";
 	
-	std::cout << "Desplazamiento vertical: " << calcularDesplazamientoVertical(cv::Point(r.x,r.y),calcularVerticeAbajoIzq(r),src) << "\n";
-	std::cout << "Desplazamiento horizontal: " << calcularDesplazamientoHorizontal(cv::Point(r.x,r.y),calcularVerticeAbajoIzq(r),src) << "\n";
+	std::cout << "Desplazamiento vertical: " << calcularDesplazamientoVertical(cv::Point(r.x,r.y),calcularVerticeAbajoIzq(r),src)<< " (" << traducirVertical(calcularDesplazamientoVertical(cv::Point(r.x,r.y),calcularVerticeAbajoIzq(r),src)) << ")\n";
+	std::cout << "Desplazamiento horizontal: " << calcularDesplazamientoHorizontal(cv::Point(r.x,r.y),calcularVerticeAbajoIzq(r),src) << " (" << traducirHorizontal(calcularDesplazamientoHorizontal(cv::Point(r.x,r.y),calcularVerticeAbajoIzq(r),src)) << ")\n";
 	
 	std::cout << "Tamaño adecuado: " << std::boolalpha << calcularTamano(r,src) << "\n";
+
+	std::cout << "+-------------ROBOT-------------+\n";
+
+	std::cout << "Ordenes: " << traducirVertical(calcularDesplazamientoVertical(cv::Point(r.x,r.y),calcularVerticeAbajoIzq(r),src)) << " y " << traducirHorizontal(calcularDesplazamientoHorizontal(cv::Point(r.x,r.y),calcularVerticeAbajoIzq(r),src)) << "\n";
 }
 
 
@@ -59,10 +90,11 @@ int main()
 	//cv::Mat src = cv::imread("../images/img1.png");
 	//cv::Mat src = cv::imread("../images/centro.png");
 	//cv::Mat src = cv::imread("../images/centro_peque.png");
-	cv::Mat src = cv::imread("../images/derecha.png");
+	//cv::Mat src = cv::imread("../images/derecha.png");
 	//cv::Mat src = cv::imread("../images/derecha_peque.png");
 	//cv::Mat src = cv::imread("../images/izquierda.png");
 	//cv::Mat src = cv::imread("../images/izquierda_peque.png");
+	cv::Mat src = cv::imread("../images/abajo_peque.png");
 	
 	if (src.empty())
 		return -1;
